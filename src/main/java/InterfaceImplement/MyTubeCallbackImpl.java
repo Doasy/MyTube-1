82d8c523c37f52a2d96a025f7ab56aa7cdd2e307
Alba Lamas <alba.v.lamas@gmail.com>
2017-11-16 23:15:13 +0100

    implement methods of MyTubeCallbackImpl

--- src/main/java/InterfaceImplement/MyTubeCallbackImpl.java
+++ src/main/java/InterfaceImplement/MyTubeCallbackImpl.java
@@ -12,12 +12,13 @@ public class MyTubeCallbackImpl extends UnicastRemoteObject implements MyTubeCal
     }
 
     @Override
-    public void notifyNewContent(String tittle) throws RemoteException{
-
+    public void notifyNewContent(String title) throws RemoteException{
+        System.out.println("New content has been added title "+title);
     }
 
     @Override
     public void notifyServerStopped() throws RemoteException {
-
+        System.out.print("Server stopped");
+        System.exit(0);
     }
 }
